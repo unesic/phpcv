ddfdae342a1184fa771a2cad02738e2560cebbf4
Uros Nesic <weez026@hotmail.com>
2020-01-30 00:27:18 +0100

    --alow-no-message

--- templates/canvas.php
+++ templates/canvas.php
@@ -3,10 +3,6 @@
         <div class="canvas-inner container">
             <?php
 
-            /**
-             * TODO: Update content on spot
-             */
-
             include_once 'views/component/display.php';
 
             ?>
